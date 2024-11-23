9357f0c1a6388f600b94ccb2585bd3ed707e3010
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-11-23 15:33:04 +0600

    All

--- src/module/car/car.route.ts
+++ src/module/car/car.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { CarControllers } from './car.controller';
 const router = express.Router();
 
-router.post('/create-car', CarControllers.createCar);
+router.post('/', CarControllers.createCar);
 router.get('/', CarControllers.getCar);
 router.get('/:carId', CarControllers.getSingleCar);
 router.delete('/:carId', CarControllers.deleteSingleCar);
